0fe78af0ee38bff278a02bf6dc344f0141fad851
MjSanoy <sanoymj040820@gmail.com>
2023-05-27 22:06:16 +0800

    changes

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,4 @@
 <!-- Footer-->
-<footer class="py-5 bg-dark">
+<footer class="py-5 bg-dark" style="background-image: linear-gradient(100deg, #fed8b1, #ffdebe);">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
         </footer>
